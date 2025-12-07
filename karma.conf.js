eafe8a2d35f969eab45ad56707e85433142ee850
pranav <nairpranavsivadasan@gmail.com>
2025-12-07 19:09:55 +0000

    karma conf added

---
+++ karma.conf.js
@@ -0,0 +1,14 @@
+module.exports = function (config) {
+  config.set({
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    browsers: ['ChromeHeadlessCI'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu']
+      }
+    },
+    reporters: ['progress', 'kjhtml'],
+    singleRun: true
+  });
+};
